6a91e4fe0703347b324132a7043807257bbfa20e
Kane <LisaxKane@gmail.com>
2023-02-14 02:14:21 -0600

    added foreverjs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { getTransactions } = require("./controllers/controller");
 const { generateTransaction } = require("./simulations/simulateTransaction");
 const moment = require("moment");
 const express = require("express");
-const Chance = require('chance');
+const Chance = require("chance");
 const { default: axios } = require("axios");
 const port = 5000;
 
@@ -11,20 +11,15 @@ var time = new Date();
 
 const app = express();
 let BusyHour = {
-  breakfast: moment(time).format("HH") > 8 && moment(time).format("HH") < 11,
-  lunch: moment(time).format("HH") > 12 && moment(time).format("HH") < 14,
-  dinner: moment(time).format("HH") > 18 && moment(time).format("HH") < 21,
+  breakfast: moment(time).format("HH") >= 8 && moment(time).format("HH") < 11,
+  lunch: moment(time).format("HH") >= 12 && moment(time).format("HH") < 14,
+  dinner: moment(time).format("HH") >= 18 && moment(time).format("HH") < 21,
 };
 let closed = moment(time).format("HH") > 0 && moment(time).format("HH") < 8;
 
-
 app.listen(port, () => startLoop());
 
-
-
-
-async function startLoop(){
-
+async function startLoop() {
   const server = setInterval(() => {
     (async () => {
       let transaction = await getTransactions();
@@ -33,42 +28,33 @@ async function startLoop(){
           `https://puce-beautiful-beaver.cyclic.app/restaurant/transactions/${transaction.firstId}`
         );
       }
-  
-      
-      
-      if(BusyHour.breakfast){
-        if(chance.d4() === 2){
+
+      if (BusyHour.breakfast) {
+        if (chance.d4() === 2) {
           console.log("from breakfast hrs");
           generateTransaction();
         }
-          
-      }
-      else if(BusyHour.lunch){
-         if (chance.d4() === 2) {
-          console.log("from lunch hrs");
-           generateTransaction();
-         }
-      }
-      else if(BusyHour.dinner){
-         if (chance.d2() >= 2) {
-          console.log("from dinner hrs");
-           generateTransaction();
-          }
-        }
-        else if(closed) {
-          console.log('from closed')
+      } else if (BusyHour.lunch) {
+        console.log("from lunch hrs");
+        if (chance.d4() == 2) {
           generateTransaction();
-        return
-        
+        }
+      } else if (BusyHour.dinner) {
+        if (chance.d2() >= 2) {
+          console.log("from dinner hrs");
+          generateTransaction();
+        }
+      } else if (closed) {
+        console.log("from closed");
+
+        return;
+      } else {
+        console.log(BusyHour.lunch);
+        console.log("from normal hrs");
+        if (chance.d10() === 7) {
+          generateTransaction();
+        }
       }
-      else  {
-        console.log('from normal hrs')
-         if (chance.d10() === 7) {
-           generateTransaction();
-         }
-      }
-      
-  
     })();
-  }, 5000);
+  }, 10000);
 }
