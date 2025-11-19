463de1d5fa680ac41b622bd10367789af192acd6
kkapkane <lisaxkane@gmail.com>
2025-11-19 16:03:40 -0600

    update

--- app.js
+++ app.js
@@ -7,40 +7,44 @@ const { default: axios } = require("axios");
 const port = 5000;
 
 var chance = new Chance();
-var time = new Date();
 
 const app = express();
-let BusyHour = {
-  breakfast: moment(time).format("HH") >= 8 && moment(time).format("HH") < 11,
-  lunch: moment(time).format("HH") >= 12 && moment(time).format("HH") < 14,
-  dinner: moment(time).format("HH") >= 18 && moment(time).format("HH") < 21,
-};
-let closed = moment(time).format("HH") > 0 && moment(time).format("HH") < 8;
 
 app.listen(port, () => startLoop());
 
 async function startLoop() {
   const server = setInterval(() => {
     (async () => {
+      // Update time and busy hours on each interval
+      let time = new Date();
+      let currentHour = parseInt(moment(time).format("HH"));
+      
+      let BusyHour = {
+        breakfast: currentHour >= 8 && currentHour < 11,
+        lunch: currentHour >= 12 && currentHour < 14,
+        dinner: currentHour >= 18 && currentHour < 21,
+      };
+      let closed = currentHour >= 0 && currentHour < 8;
+      
       let transaction = await getTransactions();
       if (transaction.length > 100) {
         axios.delete(
-          `https://puce-beautiful-beaver.cyclic.app/restaurant/transactions/${transaction.firstId}`
+          `https://restaurantbackend-9o91.onrender.com/restaurant/transactions/0`
         );
       }
 
       if (BusyHour.breakfast) {
-        if (chance.d4() === 2) {
+        if (chance.d2() === 2) {
           console.log("from breakfast hrs");
           generateTransaction();
         }
       } else if (BusyHour.lunch) {
         console.log("from lunch hrs");
-        if (chance.d4() == 2) {
+        if (chance.d2() == 2) {
           generateTransaction();
         }
       } else if (BusyHour.dinner) {
-        if (chance.d2() >= 2) {
+        if (chance.d2() >= 1) {
           console.log("from dinner hrs");
           generateTransaction();
         }
@@ -49,9 +53,8 @@ async function startLoop() {
 
         return;
       } else {
-        console.log(BusyHour.lunch);
-        console.log("from normal hrs");
-        if (chance.d10() === 7) {
+        console.log("from normal hrs (Hour: " + currentHour + ")");
+        if (chance.d4() >= 2) {
           generateTransaction();
         }
       }
--- controllers/controller.js
+++ controllers/controller.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 //fetching all foods from menu
 async function getFoods() {
   const res = await axios.get(
-    "https://puce-beautiful-beaver.cyclic.app/restaurant/menu/foods"
+    "https://restaurantbackend-9o91.onrender.com/restaurant/menu/foods"
   );
   let foodlist = res.data.map((food) => {
     return { name: food.name, price: food.price };
@@ -13,15 +13,17 @@ async function getFoods() {
 // fetching transactions
 async function getTransactions() {
   const res = await axios.get(
-    "https://puce-beautiful-beaver.cyclic.app/restaurant/transactions"
+    "https://restaurantbackend-9o91.onrender.com/restaurant/transactions"
   );
 
-  return { firstId: res.data[0]._id, length: res.data.length };
+  return {
+    length: res.data.length,
+  };
 }
 //fetching all drinks from menu
 async function getDrinks() {
   const res = await axios.get(
-    "https://puce-beautiful-beaver.cyclic.app/restaurant/menu/drinks"
+    "https://restaurantbackend-9o91.onrender.com/restaurant/menu/drinks"
   );
   let drinklist = res.data.map((drink) => {
     return { name: drink.name, price: drink.price };
@@ -31,7 +33,8 @@ async function getDrinks() {
 //send transaction
 async function sendTransactions(name, qty, price, order) {
   const res = await axios.post(
-    "https://puce-beautiful-beaver.cyclic.app/restaurant/transactions", {
+    "https://restaurantbackend-9o91.onrender.com/restaurant/transactions",
+    {
       customerName: name,
       totalQty: qty,
       totalPrice: price,
